90c165546c27eeb9891ed9be445b0ea97e435572
vicente.espineira <vicente.espineira@gmail.com>
2023-03-14 13:26:55 +0100

    libreria

---
+++ src/main/java/es/mde/acing/ProyectoLibVicenteecApplication.java
@@ -0,0 +1,9 @@
+package es.mde.acing;
+
+public class ProyectoLibVicenteecApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/es/mde/acing/utils/pruebaLibreria.java
@@ -0,0 +1,9 @@
+package es.mde.acing.utils;
+
+public class pruebaLibreria {
+	
+	public void imprime() {
+		System.out.println("hola libreria");
+	}
+
+}
